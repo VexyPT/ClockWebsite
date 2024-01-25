e15f17cca86369936d5f7dfe0d6bbdcb0e5a26e8
yVexy <danifilipe221205@gmail.com>
2024-01-25 16:41:57 +0000

    some javascript code

---
+++ src/script.js
@@ -0,0 +1,15 @@
+function displayTime() {
+    let d = new Date();
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+    let amOrPm = "AM"
+
+    document.getElementById("clock").innerHTML = `${hour}:${min}:${sec}`;
+}
+
+setInterval(displayTime, 1000);
+
+//log 
+let log = console.log.bind(document)
+log(`sim ${5}`)
